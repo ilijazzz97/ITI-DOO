9efb92aa4740696a6c1eecd30d0e16dec92336a7
ilijazzz97 <33433049+ilijazzz97@users.noreply.github.com>
2023-10-15 17:15:56 +0200

    Dodavanje fajlova u repozitorijum

---
+++ Controllers/VestiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITI_DOO.Controllers
+{
+    public class VestiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
