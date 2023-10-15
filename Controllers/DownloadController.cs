2fd7fd4cf6a98c8f5203d1448be500a104d77497
ilijazzz97 <33433049+ilijazzz97@users.noreply.github.com>
2023-10-15 17:12:56 +0200

    Dodavanje fajlova u repozitorijum

---
+++ Controllers/DownloadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITI_DOO.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/KontaktController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITI_DOO.Controllers
+{
+    public class KontaktController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
