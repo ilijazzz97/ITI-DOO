99c4538b2fc49fff60dcca897c154d14dbdb6ccc
ilijazzz97 <33433049+ilijazzz97@users.noreply.github.com>
2023-10-15 17:14:55 +0200

    Brisanje fajlova

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ITI_DOO.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
